48d5c36cb8e6ad757b31632dabd4cc3ce7c72226
Wendy <honeykatie@qq.com>
2017-12-10 19:45:11 -0800

    Init Upload for original O2OPlus Website

---
+++ scripts/clickintent.js
@@ -0,0 +1,18 @@
+/*
+ HTML5 点击事件
+*/
+function herf(i){
+ if(i==1){
+	window.location.href="http://www.o2oplus.com.cn/index.html";
+ }else if(i==2){
+	window.location.href="http://www.o2oplus.com.cn/p2_fruitveggie.html";
+ }else if(i==3){
+	window.location.href="http://www.o2oplus.com.cn/p1_superapp.html";
+ }else if(i==4){
+	window.location.href="http://www.o2oplus.com.cn/p5_MobileGovernment.html";
+ }else if(i==5){
+	window.location.href="http://www.o2oplus.com.cn/p4_IoT.html";
+ }else if(i==6){
+	window.location.href="http://www.o2oplus.com.cn/p3_smartcity.html";
+ }
+}
